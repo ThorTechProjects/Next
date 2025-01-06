a583b4e07c38add1a06a51af02183aa28f2bb3b4
OCs <ozsvath.csongor@gmail.com>
2025-01-07 00:30:02 +0100

    icon2

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/ThorTech.ico" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <title>KLM Monthly Work</title>
       </head>
       <body>
